1fcb787649e18f6cd2f8f52a77ec6f45ad5a8f97
Jussi Kurikka <jussi.kurikka@gmail.com>
2020-05-04 23:44:34 +0300

    Add main code and edit getData script
    - Add example main code for spark
    - Make getData.py better

--- getData.py
+++ getData.py
@@ -15,28 +15,26 @@ day_number  = 1 - 365 [Mukana hyppyvuosia]
 '''
 start = time.time()
 
-#LAM ID's in Pohjois Pohjanmaa Area
-lam_id_list_ppohjanmaa =  [1052, 1101, 1103, 1104, 1105, 1121, 1123, 1124, 1201, 1202, 1203, 1204, 1205, 1221, 1222, 1223, 1224, 1225, 1226, 1227, 1228, 1229,
-                    1230, 1232, 1233, 1234, 1235, 1236, 1237, 1238, 1239, 1243, 1244, 1246, 1247, 1248, 1249, 1250, 1251, 1252, 1253, 1254, 1255, 1246,
-                    1257, 1301, 1302, 1303, 1321, 1322, 1323, 1324, 1325, 1326, 1327, 1328, 1329]
-                    
-                   
+# LAM ID's in Pohjois Pohjanmaa Area
+lam_id_list_ppohjanmaa =  [1052, 1101, 1103, 1104]
 
+name_of_the_file="lam_data_ppohjanmaa_1.csv"
+f = open(name_of_the_file, "w")  # create a file new file if the file does not exists, deletes old file if it exists
+f.close()
 
-f= open("test_oulu_day1.txt","w+")  #create a file
+f = open(name_of_the_file, "a+")  # open the empty file in append mode
+f.write("lam_point_id;year;day;hour;minute;second;100th_second;length;lane;direction;category;speed;faulty;total_time;interval;queue_start\n")
 counter = 0
 
-for day in range(1,115): #how many days has elapsed this year
+for day in range(1,124): #how many days has elapsed this year
 
-    for number in lam_id_list_ppohjanmaa: #Goes through all LAM points
+    for number in lam_id_list_ppohjanmaa: # Goes through all LAM points
         target_url = "https://aineistot.vayla.fi/lam/rawdata/2020/12/lamraw_{}_20_{}.csv".format(number, day) #get data
-        
-        f= open("test_oulu_day1.txt","a+")  #append data to file
         
         data = urllib.urlopen(target_url)   # it's a file like object and works just like a file
         for line in data:                   # files are iterable
-            f.write(line.decode("utf-8"))   #changing value from binary to string
-            counter += 1                    #counts how many datapoints are saved
+            f.write(line.decode("utf-8"))   # changing value from binary to string
+            counter += 1                    # counts how many datapoints are saved
             
             if counter == 1000:
                 print("1K")
@@ -77,10 +75,6 @@ for day in range(1,115): #how many days has elapsed this year
             
             
 f.close()
-    
-
-   
-
 
 
 '''
